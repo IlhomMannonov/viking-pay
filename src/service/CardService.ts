c3b3933d540b72ad14b5f2befd370c77d8c81796
ilhommannonov <ilhom.mannonov.dev@gmail.com>
2025-06-07 23:23:07 +0500

    carta yaratishda eski kartalarni tekshirish

--- src/service/CardService.ts
+++ src/service/CardService.ts
@@ -5,11 +5,42 @@ import {HttpStatus} from "../entity/ResponseData/HttpStatus";
 import {__} from "i18n";
 import logger from "../config/logger";
 import {get_tg_cards, getCardInfoByNumber} from "../controller/TelegramAccountController";
+import {Transaction} from "../entity/Transaction";
+import {In} from "typeorm";
+import {getTransTime} from "../controller/TransactionController";
 
 const cardRepository = AppDataSource.getRepository(Card);
+const transactionRepository = AppDataSource.getRepository(Transaction);
 
 
 export async function getAvailableCard(amount: number,) {
+
+    const pending_cards = await cardRepository.find({where: {status: "pending_pay", deleted: false}})
+    const cards_id = pending_cards.map(card => card.id)
+    const trs = await transactionRepository.find({
+        where: {
+            card_id: In(cards_id),
+            deleted: false,
+            status: "pending_deposit"
+        }
+    })
+    let card_ids:number[] = []
+
+    trs.map(transaction => {
+        let timer = 0
+        timer =  getTransTime(transaction)
+        if (timer===0){
+            card_ids.push(Number(transaction.card_id))
+        }
+    })
+
+    await AppDataSource
+        .createQueryBuilder()
+        .update(Card)
+        .set({ status: 'active' })
+        .whereInIds(card_ids)
+        .execute()
+
     const card = await cardRepository
         .createQueryBuilder("card")
         .leftJoinAndSelect("card.tg_account", "tg_account")
