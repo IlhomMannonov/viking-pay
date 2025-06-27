2dc128bd8a4e87791f3f82ee564a3875f8172a0f
ilhommannonov <ilhom.mannonov.dev@gmail.com>
2025-06-27 19:57:09 +0500

    q

--- src/middilwares/errorHandlers.ts
+++ src/middilwares/errorHandlers.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from 'express';
 import {RestException} from './RestException';
 import {HttpStatus} from '../entity/ResponseData/HttpStatus';
+import logger from "../config/logger";
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction): void => {
     if (err instanceof RestException) {
@@ -13,9 +14,13 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
             code: err.errorCode
         });
     } else {
+        logger.error(
+            `${req.method} ${req.originalUrl} - ${err.message} \nStack: ${err.stack}`
+        )
         console.error('Unexpected error:', err);
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            message: 'Internal server error',
-        });
+        res.status(err.status || 500).json({
+            success: false,
+            message: err.message || 'Internal Server Error'
+        })
     }
 };
